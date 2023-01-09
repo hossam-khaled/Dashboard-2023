e443ea4c16e279577a6c48d6160bcd1ff43c7c77
Hossam <hossam.hmiq@gmail.com>
2023-01-09 07:53:16 +0200

    add featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,52 @@
-import './featured.scss'
-
+import "./featured.scss";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 const Featured = () => {
   return (
-    <div className='featured'>Featured</div>
-  )
-}
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertOutlinedIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$430</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be included
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itrmTitle">Target</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itrmTitle">Last Week</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itrmTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section></section>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;

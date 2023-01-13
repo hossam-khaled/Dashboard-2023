3194029e4d0f9469c5421b238c7fc2da0e0b845f
Hossam <hossam.hmiq@gmail.com>
2023-01-13 19:46:00 +0200

    .

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,14 +1,15 @@
 import "./list.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
- 
+import Datatable from "../../components/datatable/Datatable";
+
 const List = () => {
   return (
     <div className="list">
       <Sidebar />
       <div className="listContainer">
         <Navbar />
-        datatable
+        <Datatable />
       </div>
     </div>
   );
